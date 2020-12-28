7ebb357179ce4792935babf44edb810617c1eb92
david bess <bessdavid93@gmail.com>
2020-12-28 17:34:57 -0600

    int lesson

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
 
     public static void main(String[] args){
         System.out.println("Hello David this is a test to make sure my commits tracked");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
     }
 }
